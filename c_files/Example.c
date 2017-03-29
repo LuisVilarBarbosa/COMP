fd80b96ccb35ac13e6b172026ce2474052413851
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2017-03-29 11:28:32 +0100

    Example.c is now like the example to test everything

--- c_files/Example.c
+++ c_files/Example.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-
 int main() {
 	#pragma tuner explore STEP(1,10) reference(STEP=1)
 	unsigned int i;
-	for (i = 1; i <= 10; i++)
-		printf("%u - Working\n", i);
+	for (int i = 0; i < N; i += STEP) {
+		acc += buf[i];
+	}
+	acc *= STEP;
 	#pragma tuner max_abs_error acc 5
 	return 0;
 }
