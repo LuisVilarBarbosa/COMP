c2813e8b792ff683b54a52f4b857dc533b57f08c
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2017-03-29 11:26:48 +0100

    Lexical analyser bug fix (based on what was done in practical class)

--- LexicalAnalyser.java
+++ LexicalAnalyser.java
@@ -30,20 +30,25 @@ public class LexicalAnalyser {
 
         Vector<Token> tokens = new Vector<>();
         for (int i = 0; i < lines.size(); i++) {
-            StringTokenizer st = new StringTokenizer(lines.get(i), " 	", false);
-            while (st.hasMoreTokens()) {
-                String t = st.nextToken();
-                Token token;
+            StringTokenizer st1 = new StringTokenizer(lines.get(i), " \t", false);
+            while (st1.hasMoreTokens()) {
+                String t1 = st1.nextToken();
+                StringTokenizer st2 = new StringTokenizer(t1, "(,)=", true);
 
-                Matcher m1 = VAR.matcher(t);
-                Matcher m2 = INT.matcher(t);
-                Matcher m3 = FLOAT.matcher(t);
-                if (m1.matches()) token = new Token(t, Token.VAR_TYPE);
-                else if (m2.matches()) token = new Token(t, Token.INT_TYPE);
-                else if (m3.matches()) token = new Token(t, Token.FLOAT_TYPE);
-                else token = new Token(t, Token.UNDEFINED_TYPE);
+                while (st2.hasMoreTokens()) {
+                    String t2 = st2.nextToken();
+                    Token token;
 
-                tokens.add(token);
+                    Matcher m1 = VAR.matcher(t2);
+                    Matcher m2 = INT.matcher(t2);
+                    Matcher m3 = FLOAT.matcher(t2);
+                    if (m1.matches()) token = new Token(t2, Token.VAR_TYPE);
+                    else if (m2.matches()) token = new Token(t2, Token.INT_TYPE);
+                    else if (m3.matches()) token = new Token(t2, Token.FLOAT_TYPE);
+                    else token = new Token(t2, Token.UNDEFINED_TYPE);
+
+                    tokens.add(token);
+                }
             }
         }
         return tokens;
