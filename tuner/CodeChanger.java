7d9b8679990de4c4ce285d83cc6ecddcae63da6e
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2017-05-16 17:23:40 +0100

    Started to add code to add to the C code
    Done on March 15, 2017 between 14h00 and 15h10.
    - Added two FOR cycles of C and a printf() to be added in the scope of
    each pragma.
    - Added a "\n" in the end of each C code line that was missing.
    - Probably the C code that is to add will be removed from the Java code
    and stored in files that are loaded and some parts of the code loaded
    will be adjusted and then added to the C code.

--- tuner/CodeChanger.java
+++ tuner/CodeChanger.java
@@ -16,49 +16,63 @@ public class CodeChanger {
         this.HIRs = HIRs;
     }
 
-    /**
-     * Testa diferentes versoes do codigo.
-     *
-     * @throws IOException
-     * @throws InterruptedException
-     */
-    public void codeVariantsTest() throws IOException, InterruptedException {
-        ArrayList<String> codeChanged;
-        while ((codeChanged = changeCCode()) != null) {
-            generateFileWithCode(codeChanged);
-            CodeExecutor codeExecutor = new CodeExecutor(testCodeFile);
-            codeExecutor.compile();
-            long runTime = run(codeExecutor);
-            System.out.println("Running time: " + runTime / Math.pow(10, 6) + " milliseconds");
 
-            codeExecutor.delete();
-        }
+    public void codeVariantsTest() throws IOException, InterruptedException {
+        ArrayList<String> codeChanged = changeCCode();
+        generateFileWithCode(codeChanged);
+        CodeExecutor codeExecutor = new CodeExecutor(testCodeFile);
+        codeExecutor.compile();
+        long runTime = run(codeExecutor);
+        System.out.println("Running time: " + runTime / Math.pow(10, 6) + " milliseconds");
+        codeExecutor.delete();
     }
 
-    /**
-     * TODO
-     *
-     * @return
-     */
     private ArrayList<String> changeCCode() {
         ArrayList<String> codeChanged = codeLines;
-        boolean changed = false;
 
-        for (int i = 0, j = pragmaIndexes.size() - 1; i <= j; i++, j--) {
-            for (int k = 1; k < codeChanged.size() - 1; k++) {
-                // update "changed" and change the code looking to the semantic analysis data
+        for (int i = 0; i < HIRs.size(); i++) {
+            int scopeBegin = pragmaIndexes.get(i) + 1;
+            int scopeEnd = pragmaIndexes.get(pragmaIndexes.size() - 1 - i);
+            ArrayList<Node> children = HIRs.get(i).getChildren();
+
+            if (children.size() >= 2) {
+                Node n1 = children.get(0);
+                Node n2 = children.get(1);
+                if (n1.getInfo().equals("explore") && n2.getInfo().equals("max_abs_error")) {
+                    ArrayList<Node> exploreChildren = n1.getChildren();
+                    String varName = exploreChildren.get(0).getInfo();
+                    ArrayList<Node> varChildren = exploreChildren.get(0).getChildren();
+                    String startValue = varChildren.get(0).getInfo();
+                    String endValue = varChildren.get(1).getInfo();
+                    String tunerVarName = "_TUNER_VAR_" + varName;
+                    String tunerRepeatVarName = "_TUNER_REPEAT_VAR_" + varName;
+
+                    ArrayList<String> newStartCode = new ArrayList<>();
+                    newStartCode.add("for (float " + tunerVarName + " = " + startValue + "; " + tunerVarName + " < " + endValue + "; " + tunerVarName + "++" + ") {");
+                    newStartCode.add("for (int " + tunerRepeatVarName + " = 1; " + tunerRepeatVarName + " < 5; " + tunerRepeatVarName + "++) {");
+
+                    String time = "1";  // to remove
+                    ArrayList<String> newEndCode = new ArrayList<>();
+                    newEndCode.add("}");
+                    newEndCode.add("printf(\"" + varName + "_%lf_%lf\\n\", " + tunerVarName + ", " + time + " / 5.0" + ");");
+                    newEndCode.add("}");
+
+                    codeChanged.addAll(scopeEnd, newEndCode);
+                    codeChanged.addAll(scopeBegin, newStartCode);
+                }
             }
         }
 
-        if (changed)
-            return codeChanged;
-        return null;
+        return codeChanged;
     }
 
     private void generateFileWithCode(ArrayList<String> code) throws IOException {
         FileOutputStream file = new FileOutputStream(testCodeFile);
-        for (String line : code)
+        byte[] endLine = "\n".getBytes();
+        for (String line : code) {
             file.write(line.getBytes());
+            file.write(endLine);
+        }
         file.close();
     }
 
