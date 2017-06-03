78b05f564213259739574ae71c3406a3e63767f6
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2017-06-03 22:28:09 +0100

    'ComplexExample.c' updated - Auto ready for him

--- c_files/ComplexExample.c
+++ c_files/ComplexExample.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
-// pragmas inside this function are commented because Auto is not ready to read pragmas by function
 void function() {
 	int i, VAR, acc = 0;
-	// #pragma tuner explore VAR(1,10) reference(VAR=1)
+	#pragma tuner explore VAR(1,10) reference(VAR=1)
 	for (i = VAR; i < 10; i++)
 		acc += VAR;
 	acc = acc + VAR;
-	// #pragma tuner max_abs_error acc 10
+	#pragma tuner max_abs_error acc 10
 }
 
 int main() {
-	int i, N = 10, acc = 0, acc2 = 0;
+	int i, N = 10, STEP, VAR, acc = 0, acc2 = 0;
 	int buf[] = {1,2,3,4,5,6,7,8,9,10};
 	#pragma tuner explore STEP(1,10) reference(STEP=1)
 	#pragma tuner explore VAR(1,10) reference(VAR=1)
