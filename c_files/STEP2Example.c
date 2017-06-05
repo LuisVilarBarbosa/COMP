5b75a6aa446d1cb5ddc572d29382966d6b8b1b82
Diogo Cruz <diogohmcruz@hotmail.com>
2017-06-05 18:51:50 +0100

    Ficheiro de teste incremento 2

---
+++ c_files/STEP2Example.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	int STEP;
+	int acc = 2;
+  #pragma tuner explore STEP(1,10,2) reference(STEP=1)
+	unsigned int i;
+	int N = 10;
+	int buf[10] = {2}; 
+	for (i = 0; i < N; i = i + STEP) {
+		acc = acc + buf[i];
+	}
+	acc = acc + STEP;
+	#pragma tuner max_abs_error acc 5
+	return 0;
+}
