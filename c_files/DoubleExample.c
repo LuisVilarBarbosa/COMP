fc4c537e82616fa10737b3f95df39f88ad44c91d
Diogo Cruz <diogohmcruz@hotmail.com>
2017-06-05 17:12:06 +0100

    update no Ficheiro double

--- c_files/DoubleExample.c
+++ c_files/DoubleExample.c
@@ -2,8 +2,8 @@
 
 int main() {
 	double STEP;
-	double acc = 2.0;
-	#pragma tuner explore STEP(1,10) reference(STEP=1)
+	double acc = 2;
+	#pragma tuner explore STEP(1.2,10.2) reference(STEP=2.4)
 	unsigned int i;
 	int N = 10;
 	int buf[10] = {2}; 
